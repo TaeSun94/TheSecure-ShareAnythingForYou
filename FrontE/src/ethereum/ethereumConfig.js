19dbfe96af77e2cbaa8a127042591eaaea2348cc
tyzlddy <tyzlddy@naver.com>
2020-10-07 14:06:28 +0900

    [+] Ethereum web3 modules

---
+++ FrontE/src/ethereum/ethereumConfig.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.coinbase = {
+    address: '0xd7a016eef56913402afe2e61e5067fbdda427e81',
+    password: 'ssafy'
+}
+
+config.host = {
+    address: '0xdd8d6da41c3199297c9af36fc263467babe5f86a',
+    password: 'ssafy'
+}
+
+config.reservation = {
+    address: '0xD79A3677C7729286393761435DaE50B74B9C1769',
+    password: 'ssafy'
+}
+
+export default config;
