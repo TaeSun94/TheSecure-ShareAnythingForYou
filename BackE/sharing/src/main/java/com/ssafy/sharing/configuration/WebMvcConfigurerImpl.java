f2377ad72575e50f1853d9fc9cca60a2a8b1fcef
hanbyeol123 <gksquf4056@gmail.com>
2020-10-07 19:03:28 +0900

    no message

--- BackE/sharing/src/main/java/com/ssafy/sharing/configuration/WebMvcConfigurerImpl.java
+++ BackE/sharing/src/main/java/com/ssafy/sharing/configuration/WebMvcConfigurerImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.sharing.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfigurerImpl implements WebMvcConfigurer {
+	@Value("${image.absolute.location}")
+	private String imageLocation;
+	
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 요청에 대해서
@@ -16,7 +20,8 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	System.out.println(imageLocation);
         registry.addResourceHandler("/resources/**")
-                .addResourceLocations("file:/home/ubuntu/images/"); //리눅스 root에서 시작하는 폴더 경로
+                .addResourceLocations(imageLocation); //리눅스 root에서 시작하는 폴더 경로
     }
 }
