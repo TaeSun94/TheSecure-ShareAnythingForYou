2d4af0fd787da2530fa2be597f618dd7dd65fca0
hanbyeol123 <gksquf4056@gmail.com>
2020-10-07 23:50:42 +0900

    no message

--- BackE/sharing/src/main/java/com/ssafy/sharing/configuration/WebMvcConfigurerImpl.java
+++ BackE/sharing/src/main/java/com/ssafy/sharing/configuration/WebMvcConfigurerImpl.java
@@ -8,20 +8,16 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan
-@EnableWebMvc
 public class WebMvcConfigurerImpl implements WebMvcConfigurer {
 	
     @Override
 	public void addCorsMappings(CorsRegistry registry) {
-    	System.out.println("ㅂ듓ㅂㄷ샙ㄷㅂ대숍");
         registry.addMapping("/**") //모든 요청에 대해서
                 .allowedOrigins("*"); //허용할 오리진들
     }
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    	System.out.println("쀼뜌루쀼쀼");
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:/home/ubuntu/images/"); //리눅스 root에서 시작하는 폴더 경로
     }	
